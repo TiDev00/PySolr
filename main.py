73adcef7a5e5ec3fedf090c8a33037b913627256
TiDev00 <cisse.thierno@outlook.com>
2021-12-12 18:16:25 -0500

    tf-idf for multiple lines

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
 import contextlib
-
 import nltk
 import pandas as pd
 from gensim.parsing.preprocessing import remove_stopwords as stopwords
 import pysolr
 import csv
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def query_from_file(my_file):
-    # initialize the parameters
-    host = "localhost"
-    port = "8983"
-    core = "Process_BM25"
-    qt = "select"
-    op = ""
-    sort = "score desc"
-    rows = "50"
-    query_field = "docno,text"
-    query_expansion_field = "docno,score"
-    df = "text"
+# initialize the parameters
+host = "localhost"
+port = "8983"
+core = "Process_BM25"
+qt = "select"
+op = ""
+sort = "score desc"
+rows = "30"
+query_field = "text"
+expanded_query_field = "docno, score"
+df = "text"
 
-    # construct the url
-    url = 'http://' + host + ':' + port + '/solr/' + core
+# construct the url
+url = 'http://' + host + ':' + port + '/solr/' + core
 
-    # construct the query
-    solr_client = pysolr.Solr(url, search_handler="/" + qt, timeout=5)
+# construct the query
+solr_client = pysolr.Solr(url, search_handler="/" + qt, timeout=5)
 
+# Extract the relevant words by calculating their weight using tf*idf
+def weighting(top_texts):
+    relevant_words = []
+    # Remove the stop words
+    filtered_sentences = []
+    for sentence in top_texts:
+        filtered_sentences.append(stopwords(sentence))
+
+    # Ponderation of each term in the set
+    tfidf = TfidfVectorizer(use_idf=True)
+    tf_idf = tfidf.fit_transform(filtered_sentences)
+    data_frame = pd.DataFrame(tf_idf[0].T.todense(), index=tfidf.get_feature_names_out(), columns=["score"])\
+        .sort_values(by='score', ascending=False)\
+        .head(25)
+    print(data_frame)
+
+# Run the expanded query
+def expanded_query(num_q,query,relevant_words):
+    print()
+# Add the rank key to the docs and print line in trec_eval format
+# for document in documents:
+#     document.update({'rank': documents.index(document) + 1})
+#     # redirect output to a file
+#     with open('output.txt', 'a') as external_file:
+#         with contextlib.redirect_stdout(external_file):
+#             print(line[0], "\tQ0", document['docno'][0], "\t", document['rank'], "\t",
+#                   "{:.6f}".format(document['score']), "\tSTANDARD")
+#     external_file.close()
+
+# Run the queries from a csv file
+def query(my_file):
     with open(my_file, "r") as f:
         reader = csv.reader(f, delimiter=";")
         next(reader, None)
@@ -42,52 +70,23 @@ def query_from_file(my_file):
             })
 
             # Number of docs found for the query
-            # print(query_response.hits, "documents found for the query number", line[0])
+            print(query_response.hits, "documents found for the query number", line[0])
 
             # Get the docs of the response
             documents = query_response.docs
 
-            # Extract the 20 first docs (top 20)
-            top_docs = []
-            for index in range(30):
-                top_docs.append(documents[index])
-
-            # Create a dataset for each text
-            for top_doc in top_docs:
-                dataset = []
-
-            # Replace every \n in text and convert all text to Lower Case
-                for text in top_doc['text']:
+            # extract all the 30 texts and convert to lower case
+            top_texts = []
+            for doc in documents:
+                for text in doc['text']:
                     text = text.replace('\n', ' ')
-                    dataset.append(text.lower())
+                    top_texts.append(str(text).lower())
 
-            # Tokenize the sentences
-                    tokens = nltk.sent_tokenize(dataset[0])
+            # Find relevant word in the top texts
+            words = weighting(top_texts)
 
-            # Remove stop words
-                    filtered_sentences = []
-                    for sentence in tokens:
-                        filtered_sentences.append(stopwords(sentence))
-
-            # Ponderation of each term in a document
-                    TFIDF = TfidfVectorizer(use_idf=True)
-                    tf_idf = TFIDF.fit_transform(filtered_sentences)
-                    print(tf_idf)
-                    # df = pd.DataFrame(tf_idf[0].T.todense(), index=TFIDF.get_feature_names_out(), columns=["TF-IDF"])
-                    # df = df.sort_values('TF-IDF', ascending=False)
-                    # print(df.head(25))
+            # Run the expanded query with relevant words
+            # expanded_query(line[0],line[1],words)
 
 
-
-            # Add the rank key to the docs and print line in trec_eval format
-            # for document in documents:
-            #     document.update({'rank': documents.index(document) + 1})
-            #     # redirect output to a file
-            #     with open('output.txt', 'a') as external_file:
-            #         with contextlib.redirect_stdout(external_file):
-            #             print(line[0], "\tQ0", document['docno'][0], "\t", document['rank'], "\t",
-            #                   "{:.6f}".format(document['score']), "\tSTANDARD")
-            #     external_file.close()
-
-
-query_from_file("mini_duplicate.csv")
+query("duplicate.csv")
