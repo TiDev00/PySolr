be3cbcd976c804b00efd7ccf554b3e6b532c4533
TiDev00 <cisse.thierno@outlook.com>
2021-12-12 19:31:20 -0500

    pseudo relevance feedback

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import contextlib
-import nltk
 import pandas as pd
 from gensim.parsing.preprocessing import remove_stopwords as stopwords
 import pysolr
 import csv
 from sklearn.feature_extraction.text import TfidfVectorizer
-
 
 # initialize the parameters
 host = "localhost"
@@ -14,7 +12,8 @@ core = "Process_BM25"
 qt = "select"
 op = ""
 sort = "score desc"
-rows = "30"
+query_rows = "30"
+expanded_query_rows = "1000"
 query_field = "text"
 expanded_query_field = "docno, score"
 df = "text"
@@ -25,37 +24,68 @@ url = 'http://' + host + ':' + port + '/solr/' + core
 # construct the query
 solr_client = pysolr.Solr(url, search_handler="/" + qt, timeout=5)
 
+
 # Extract the relevant words by calculating their weight using tf*idf
 def weighting(top_texts):
+    # Initialize the relevant words list
     relevant_words = []
+
     # Remove the stop words
     filtered_sentences = []
     for sentence in top_texts:
         filtered_sentences.append(stopwords(sentence))
 
     # Ponderation of each term in the set
-    tfidf = TfidfVectorizer(use_idf=True)
-    tf_idf = tfidf.fit_transform(filtered_sentences)
-    data_frame = pd.DataFrame(tf_idf[0].T.todense(), index=tfidf.get_feature_names_out(), columns=["score"])\
-        .sort_values(by='score', ascending=False)\
+    first_vector = TfidfVectorizer(use_idf=True)
+    tf_idf = first_vector.fit_transform(filtered_sentences)
+    data_frame = pd.DataFrame(tf_idf[0].T.todense(), first_vector.get_feature_names_out(), columns=["score"]) \
+        .sort_values(by='score', ascending=False) \
         .head(25)
-    print(data_frame)
+    data_frame.to_csv('words.csv', sep=';', index=True, header=False)
+
+    # updating the relevant list
+    with open('words.csv', "r") as file:
+        reader = csv.reader(file, delimiter=";")
+        for line in reader:
+            relevant_words.append(str(line[0]))
+    return relevant_words
+
 
 # Run the expanded query
-def expanded_query(num_q,query,relevant_words):
-    print()
+def expanded_query(num_q, query, relevant_words):
+    # Construct the expanded query
+    expanded_query = query
+    for words in relevant_words:
+        expanded_query += " " + words + " "
+
+    # Search and get the new response
+    expanded_query_response = solr_client.search(expanded_query, **{
+        'q.op': op,
+        'sort': sort,
+        'rows': expanded_query_rows,
+        'fl': expanded_query_field,
+        'df': df,
+    })
+
+    # Number of docs found for the query
+    print(expanded_query_response.hits, "documents found for the query number", num_q)
+
+    # Get the docs of the response
+    documents = expanded_query_response.docs
+
 # Add the rank key to the docs and print line in trec_eval format
-# for document in documents:
-#     document.update({'rank': documents.index(document) + 1})
-#     # redirect output to a file
-#     with open('output.txt', 'a') as external_file:
-#         with contextlib.redirect_stdout(external_file):
-#             print(line[0], "\tQ0", document['docno'][0], "\t", document['rank'], "\t",
-#                   "{:.6f}".format(document['score']), "\tSTANDARD")
-#     external_file.close()
+    for document in documents:
+        document.update({'rank': documents.index(document) + 1})
+        # redirect output to a file
+        with open('expanded_query.txt', 'a') as external_file:
+            with contextlib.redirect_stdout(external_file):
+                print(num_q, "\tQ0", document['docno'][0], "\t", document['rank'], "\t",
+                    "{:.6f}".format(document['score']), "\tSTANDARD")
+        external_file.close()
+
 
 # Run the queries from a csv file
-def query(my_file):
+def searcher(my_file):
     with open(my_file, "r") as f:
         reader = csv.reader(f, delimiter=";")
         next(reader, None)
@@ -64,13 +94,10 @@ def query(my_file):
             query_response = solr_client.search(q, **{
                 'q.op': op,
                 'sort': sort,
-                'rows': rows,
+                'rows': query_rows,
                 'fl': query_field,
                 'df': df,
             })
-
-            # Number of docs found for the query
-            print(query_response.hits, "documents found for the query number", line[0])
 
             # Get the docs of the response
             documents = query_response.docs
@@ -86,7 +113,7 @@ def query(my_file):
             words = weighting(top_texts)
 
             # Run the expanded query with relevant words
-            # expanded_query(line[0],line[1],words)
+            expanded_query(line[0], line[1], words)
 
 
-query("duplicate.csv")
+searcher("long_q.csv")
